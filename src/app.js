7c37a29e24f6b51c42e45f9cd02f535070358742
anuvedraju <anuvedraju@gmail.com>
2025-03-06 23:12:03 +0530

    taskmanagerserverproject

--- src/app.js
+++ src/app.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-// const apiRoutes = require('./routes/chat'); // Ensure correct import
-const authRoutes = require('./routes/authorizationroutes'); // Ensure correct import
-const taskRoutes = require('./routes/taskroutes'); // Ensure correct import
-// const searchRoutes = require('./routes/searchRoutes'); // Ensure correct import
-// const notificationRoutes = require('./routes/notificationroute'); // Ensure correct import
+const authRoutes = require('./routes/authorizationroutes');
+const taskRoutes = require('./routes/taskroutes');
+
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());  // Make sure express.json() middleware is here!
+app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);  // Auth routes
-app.use('/api/task', taskRoutes);  // Auth routes
-// app.use('/api/search', searchRoutes);  // Auth routes
-// app.use('/api/notify', notificationRoutes);  // Auth routes
+app.use('/api/auth', authRoutes); 
+app.use('/api/task', taskRoutes); 
+
 
 
 
